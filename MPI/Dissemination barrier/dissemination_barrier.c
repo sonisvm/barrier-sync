f19277d5c5dce3298b0c3c30f2cbc9c89c2512f6
Sonia Mathew <sonisvm@gmail.com>
2018-10-20 14:41:14 -0400

    corrected dissemination algo and took measurements again

--- MPI/Dissemination barrier/dissemination_barrier.c
+++ MPI/Dissemination barrier/dissemination_barrier.c
@@ -10,12 +10,13 @@ int *partner_flags;
 
 int num_rounds;
 
-void dissemination_barrier() {
+void dissemination_barrier(int pid) {
 	int send_buff;
 	int rec_buff;
 	MPI_Status stat;
 
 	for(int i = 0; i < num_rounds; i++) {
+		printf("sending message from %d to %d\n", pid, partner_flags[i]);
 		MPI_Send(&send_buff, 1, MPI_INT, partner_flags[i], i, MPI_COMM_WORLD);
 		MPI_Recv(&rec_buff, 1, MPI_INT, MPI_ANY_SOURCE, i, MPI_COMM_WORLD, &stat);
   	}
@@ -23,29 +24,31 @@ void dissemination_barrier() {
 
 int main(int argc, char *argv[]) {
 	int init;
-  	num = atoi(argv[1]);
+  	int BARRIER_COUNT = atoi(argv[1]);
 
 	init = MPI_Init(&argc, &argv);
 	if (init != MPI_SUCCESS) {
 		MPI_Abort(MPI_COMM_WORLD, init);
 	}
 
-	//dissemination_barrier_init();
-  	MPI_Comm_size(MPI_COMM_WORLD, &num);
+	int num_process ;
+	// Get the number of processes
+    	MPI_Comm_size(MPI_COMM_WORLD, &num_process);
+
   	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  	num_rounds = (int) ceil(log2(num));
+  	num_rounds = (int) ceil(log2(num_process));
   	partner_flags = (int*) calloc(num_rounds, sizeof(int));
   	int ex = 1;
 
  	for(int i = 0; i < num_rounds; i++) {
-      		partner_flags[i] = (rank + ex) % num;
+      		partner_flags[i] = (rank + ex) % num_process;
       		ex = ex * 2;
   	}
 	
 	struct timeval start, end;
 	gettimeofday(&start, NULL);
 
-  	for(int j = 0; j < num; j++) {
+  	for(int j = 0; j < BARRIER_COUNT; j++) {
     		dissemination_barrier(rank);
   	}
 
@@ -53,7 +56,7 @@ int main(int argc, char *argv[]) {
 	MPI_Barrier(MPI_COMM_WORLD);
 
 	printf("Time taken for process %d = %lf\n", rank, (double)((end.tv_sec * 1000000 + end.tv_usec)
-                  - (start.tv_sec * 1000000 + start.tv_usec))/num);
+                  - (start.tv_sec * 1000000 + start.tv_usec))/BARRIER_COUNT);
 
   	MPI_Finalize();
 	return 0;
